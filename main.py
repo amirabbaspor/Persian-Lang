8714c46aa372cacafd72a490dbb7352d883d2089
amirAbbaspor <amir021a12@gmail.com>
2025-09-20 23:43:19 +0330

    work on riazi()

--- main.py
+++ main.py
@@ -6,7 +6,30 @@ with open("main.persian","r",encoding="UTF-8") as f:
 
 
 for line in main_code:
+    print("---  start")
 
-    if line.startswith("bechap"):
-        print(line[len("bechap")+1:])
+
+    
+    if "riazi(" in line :
+        first = line.index("riazi(")
+        x_ = 0 
+        founded = 0
+        end = None
+        while True:
+            if line[first+len("riazi(")+x_] == "(":
+                founded = founded + 1
+            elif line[first+len("riazi(")+x_] == ")":
+                if founded > 0:
+                    founded = founded - 1
+                else :
+                    end = first + len("riazi(") + x_
+                    break
+            x_ += 1
         
+        if end != None :
+            print(eval(line[first+len("riazi("):end]))
+
+
+            
+
+    print("---  finished")
